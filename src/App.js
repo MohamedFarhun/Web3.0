ea850d0b2200e027fd83e238d61f15565a8c027e
MohamedFarhun <mohamedfarhun.it20@bitsathy.ac.in>
2022-08-01 01:59:39 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/favicon.ico  modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/abi/abi.json 	new file:   src/assets/images/1.png 	new file:   src/assets/images/10.png 	new file:   src/assets/images/11.gif 	new file:   src/assets/images/12.jpg 	new file:   src/assets/images/2.svg 	new file:   src/assets/images/3.png 	new file:   src/assets/images/4.png 	new file:   src/assets/images/5.png 	new file:   src/assets/images/6.png 	new file:   src/assets/images/7.gif 	new file:   src/assets/images/8.png 	new file:   src/assets/images/9.png 	new file:   src/assets/images/logo.png 	new file:   src/assets/images/opensea-logo.png 	new file:   src/data/data.json 	modified:   src/index.css 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
+import { GiBoltSpellCast } from "react-icons/gi";
+import abi from "./abi/abi.json";
+import data from "./data/data.json";
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [provider, setProvider] = useState(null);
+  const [nfts, setNfts] = useState(data);
+
+  const balance = async (nft) => {
+    const contract = new ethers.Contract(nft.address, abi, provider);
+    const tempBalance = await contract.balanceOf(account);
+    const tempNfts =[...nfts.list];
+    const tempNft=tempNfts[tempNfts.findIndex((obj)=>obj.id==nft.id)];
+    tempNft.owner=tempBalance >0;
+    tempNft.count=tempBalance.toString();
+    setNfts({
+      list:tempNfts,
+    })
+  };
+
+  const checkCollection = () => {
+    data.list.forEach((nft) => {
+      balance(nft);
+    });
+  };
+
+  const initConnection = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      const tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(tempProvider);
+      setAccount(accounts[0]);
+    } else {
+      console.log("Install Metamask");
+    }
+  };
+
+  useEffect(() => {
+    initConnection();
+  }, []);
+
+  useEffect(() => {
+    checkCollection();
+  }, [account]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="page">
+      <div className="header">
+        <img src={require(`./assets/images/logo.png`)} className="artIcon" />
+        <p className="head">
+          NFTMarket web 3.0
+          <span>
+            <GiBoltSpellCast style={{ marginLeft: "5px" }} />
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {account == "" ? (
+          <button onClick={initConnection} className="button">
+            Connect
+          </button>
+        ) : (
+          <p className="p">
+            Metamask wallet contract address
+            <br />
+            {account}
+          </p>
+        )}
+      </div>
+      <div className="main">
+        {nfts.list.map((nft, index) => {
+          return (
+            <div key={index} className="card">
+              <div style={{ position: "relative" }}>
+                <a target={"_blank"} href={`${nft.link}`}>
+                  <img
+                    src={require(`./assets/images/opensea-logo.png`)}
+                    className="cardimage"
+                  />
+                </a>
+                <GiBoltSpellCast
+                  className="cardimage"
+                  style={{ opacity: nft.owner ? 1 : 0.2 }}
+                />
+              </div>
+              <p className="counter">{nft.count}</p>
+              <img
+                src={require(`./assets/images/${nft.id}.${nft.type}`)}
+                className="nftimage"
+                style={{ opacity: nft.owner ? 1 : 0.2 }}
+              />
+              <p className="nfttext">{nft.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
